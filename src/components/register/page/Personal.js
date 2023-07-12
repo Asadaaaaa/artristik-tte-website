c63f4ad4963e9a2fe31b5c0280f727cc2cc61f31
zkazharan <azka.azharan@gmail.com>
2023-07-12 15:39:00 +0700

    Feat: Register -> Data Personal

--- src/components/register/page/Personal.js
+++ src/components/register/page/Personal.js
@@ -2,10 +2,11 @@ import { useState } from 'react';
 
 import Card from '@/components/register/utils/Card';
 import Input from '@/components/register/utils/Input';
+import Button from '@/components/register/utils/Button';
 import CardTitle from '@/components/register/utils/CardTitle';
 import CardSubtitle from '@/components/register/utils/CardSubtitle';
 
-export default function Personal({ dataPersonal, setDataPersonal }) {
+export default function Personal({ dataPersonal, setDataPersonal, next }) {
   
   const [errorText, setErrorText] = useState({
     id: "",
@@ -14,6 +15,14 @@ export default function Personal({ dataPersonal, setDataPersonal }) {
     email: "",
     phone_number: "",
   });
+
+  const dataPattern = {
+    id: /^[0-9]+$/,
+    name: /^[A-Za-z]+(?:\s[A-Za-z]+)*$/,
+    birthdate: /^\d{4}-\d{2}-\d{2}$/,
+    email: /^\S+@\S+\.\S+$/,
+    phone_number: /^\+?[0-9]+$/,
+  }
 
   return (
     <Card>
@@ -32,7 +41,7 @@ export default function Personal({ dataPersonal, setDataPersonal }) {
             errorText={errorText.id}
             value={dataPersonal.id}
             onChange={(e) => {
-              if (isNaN(e.target.value)) return;
+              if (isNaN(e.target.value) || e.target.value === " ") return;
 
               setErrorText({ ...errorText, id: ""});
               setDataPersonal({ ...dataPersonal, id: e.target.value})
@@ -45,13 +54,88 @@ export default function Personal({ dataPersonal, setDataPersonal }) {
             errorText={errorText.name}
             value={dataPersonal.name}
             onChange={(e) => {
-              if (isNaN(e.target.value)) return;
-              
               setErrorText({ ...errorText, name: ""});
               setDataPersonal({ ...dataPersonal, name: e.target.value})
             }}
           />
+          <Input
+            type={"date"}
+            placeholder={"Tanggal Lahir"}
+            errorText={errorText.birthdate}
+            value={dataPersonal.birthdate}
+            onChange={(e) => {
+              setErrorText({ ...errorText, birthdate: ""});
+              setDataPersonal({ ...dataPersonal, birthdate: e.target.value })
+            }}
+          />
         </div>
+        <div className="flex flex-col gap-2 px-3">
+          <h2 className="mb-1">Kontak Personal</h2>
+          <Input
+            type={"text"}
+            placeholder={"Email"}
+            maxLength={100}
+            errorText={errorText.email}
+            value={dataPersonal.email}
+            onChange={(e) => {
+              setErrorText({ ...errorText, email: ""});
+              setDataPersonal({ ...dataPersonal, email: e.target.value})
+            }}
+          />
+          <Input
+            type={"text"}
+            placeholder={"Nomor Telepon"}
+            maxLength={20}
+            errorText={errorText.phone_number}
+            value={dataPersonal.phone_number}
+            onChange={(e) => {
+              setErrorText({ ...errorText, phone_number: ""});
+              setDataPersonal({ ...dataPersonal, phone_number: e.target.value})
+            }}
+          />
+        </div>
+        <Button
+          text={"Lanjut"}
+          onClick={() => {
+            if (dataPersonal.id.length !== 16 || !dataPattern.id.test(dataPersonal.id)) {
+              setErrorText({ ...errorText, id: "NIK Tidak Valid"});
+
+              return;
+            }
+
+            if (dataPersonal.name.length < 2) {
+              setErrorText({ ...errorText, name: "Panjang Minimum Input 2 Karakter"});
+
+              return;
+            }
+
+            if (!dataPattern.name.test(dataPersonal.name)) {
+              setErrorText({ ...errorText, name: "Input hanya terdiri dari karakter alfabet, dengan kata-kata yang dipisahkan oleh satu spasi dan tidak ada spasi yang berurutan"})
+
+              return;
+            }
+
+            if (dataPersonal.birthdate.length !== 10 || !dataPattern.birthdate.test(dataPersonal.birthdate)) {
+              setErrorText({ ...errorText, birthdate: "Tanggal Tidak Valid"});
+
+              return;
+            }
+
+            if (dataPersonal.email.length < 5 || !dataPattern.email.test(dataPersonal.email)) {
+              setErrorText({ ...errorText, email: "Email Tidak Valid"});
+
+              return;
+            }
+
+            if (dataPersonal.phone_number.length < 5 || !dataPattern.phone_number.test(dataPersonal.phone_number)) {
+              setErrorText({ ...errorText, phone_number: "No Telepon Tidak Valid"});
+
+              return;
+            }
+
+            next();
+          }}
+        />
       </div>
     </Card>
   );
--- src/components/register/utils/Input.js
+++ src/components/register/utils/Input.js
@@ -1,14 +1,58 @@
+import { useState, useRef } from "react"
+
 export default function Input({ type, placeholder, maxLength, errorText, value, onChange }) {
+
+  const [hideDateLabel, setHideDateLabel] = useState(false);
+  const dateRef = useRef(null);
+
   return (
     <div className="flex flex-col">
-      <input
-        className={"px-5 py-3 rounded-full border text-sm " + (errorText !== "" ? "border-red-500 focus:outline-red-500" : "border-slate-300 focus:outline-sky-500")}
-        type={type}
-        maxLength={maxLength}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-      />
+      {
+        type === "text" && (
+          <input
+            spellCheck={"false"}
+            className={"px-5 py-3 rounded-full border text-sm " + (errorText !== "" ? "border-red-500 focus:outline-red-500" : "border-slate-300 focus:outline-sky-500")}
+            type={type}
+            maxLength={maxLength}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+          />
+        )
+      }
+      {
+        type === "date" && (
+          <div className="relative flex items-center">
+            <input
+              ref={dateRef}
+              className={"w-full px-5 py-3 rounded-full border text-sm cursor-text " + (errorText !== "" ? "border-red-500 focus:outline-red-500" : "border-slate-300 focus:outline-sky-500")}
+              type={type}
+              value={value}
+              onChange={(e) => {
+                onChange(e);
+                
+                if (e.target.value === "") {
+                  setHideDateLabel(false);
+                }
+              }}
+              onClick={() => setHideDateLabel(true)}
+            />
+            {
+              !hideDateLabel && (
+                <div 
+                  className="absolute pr-10 bg-white text-gray-400 text-sm left-5"
+                  onClick={() => {
+                    setHideDateLabel(true);
+                    dateRef.current.focus();
+                  }}
+                >
+                  Tanggal Lahir
+                </div>
+              )
+            }
+          </div>
+        )
+      }
       <p className="text-xs text-red-500 mt-1 text-right">{errorText}</p>
     </div>
   )
